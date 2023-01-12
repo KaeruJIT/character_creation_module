3e1b51e32a3a85fd86c0aa6ef3db110f025ff983
Мария Телятникова <mariia.teliatnikova@gmail.com>
2023-01-12 12:42:30 +0300

    Add docstring. Replace the main function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import randint
 
-
 from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Return information about the damage inflicted on the enemy."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -18,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Return information about the damage being blocked."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Return information about the applied special skill."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -39,6 +41,9 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Output a description of the selected character.
+    Output the value of the functions: attack(), defense(), special().
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -50,7 +55,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = 'None' # add quotes for None
+    cmd: str = 'None'  # Add quotes for None.
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -63,8 +68,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice: str = 'None' # add quotes for None
-    char_class: str = 'None' # add quotes for None
+    """Output input lines and character descriptions.
+    Return information about the selection.
+    """
+    approve_choice: str = 'None'  # Add quotes for None.
+    char_class: str = 'None'  # Add quotes for None.
     while approve_choice != 'y':
         char_class = input(
             'Введи название персонажа, за которого хочешь играть: '
@@ -95,4 +103,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
